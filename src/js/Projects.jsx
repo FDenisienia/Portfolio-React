8630b99cc6573e57bfb17e3cc1fd5df2b23d4bf1
Federico Denisienia <federicodenisienia1@gmail.com>
2022-08-12 16:14:36 -0300

    img borradas

--- src/js/Projects.jsx
+++ src/js/Projects.jsx
@@ -9,10 +9,10 @@ function Projects(){
             <TituloComponent titulo="Projects"></TituloComponent>
 
             <section className="row projects">
-                <ProjectComponent projectName="Rick and Morty App" created="Created with Javascript and Rick and Morty API" imgProject="../src/assets/proyecto-rick.jpg" aProject="https://fdenisienia.github.io/Rick-Morty/"></ProjectComponent>
-                <ProjectComponent projectName="Cotization App" created="Created with Javascript and Api Rest" imgProject="../src/assets/proyecto-cotizador.jpg" aProject="https://fdenisienia.github.io/Cotizador/"></ProjectComponent>
-                <ProjectComponent projectName="Weather App" created="Created with ReactJs + Vite and Weather API" imgProject="../src/assets/proyecto-weather.jpg" aProject="https://fdenisienia-weather-app.netlify.app/"></ProjectComponent>
-                <ProjectComponent projectName="E-commerce" created="Created with Javascript and Bootstrap" imgProject="../src/assets/proyecto-la-plata.jpg" aProject="https://fdenisienia.github.io/la-plata-3d/"></ProjectComponent>
+                <ProjectComponent projectName="Rick and Morty App" created="Created with Javascript and Rick and Morty API" imgProject="https://i.ibb.co/HxfM3br/proyecto-rick.jpg" aProject="https://fdenisienia.github.io/Rick-Morty/"></ProjectComponent>
+                <ProjectComponent projectName="Cotization App" created="Created with Javascript and Api Rest" imgProject="https://i.ibb.co/LZgsCZc/proyecto-cotizador.jpg" aProject="https://fdenisienia.github.io/Cotizador/"></ProjectComponent>
+                <ProjectComponent projectName="Weather App" created="Created with ReactJs + Vite and Weather API" imgProject="https://i.ibb.co/ydKCFWT/proyecto-weather.jpg" aProject="https://fdenisienia-weather-app.netlify.app/"></ProjectComponent>
+                <ProjectComponent projectName="E-commerce" created="Created with Javascript and Bootstrap" imgProject="https://i.ibb.co/C6Jp3pv/proyecto-la-plata.jpg" aProject="https://fdenisienia.github.io/la-plata-3d/"></ProjectComponent>
             </section>
                     
         </div>
